5d3b281f07d22bec1bcb11338415d2b899e47493
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-09-24 12:15:46 +0200

    simple domain

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -4,6 +4,33 @@ import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
 
+
+object DB {
+
+  sealed trait Status
+  case object Landing extends Status
+  case object Starting extends Status
+  case class Flying(action: Action) extends Status
+  case class Standing(action: Action) extends Status
+
+  sealed trait Action
+  case object Pecking extends Action //dziobać
+  case object Shitting extends Action
+
+
+
+  case class Pigeon(name: String, age: Int, status: Status)
+  type PigeonId = Int
+
+  var pigeons = Map[PigeonId, Pigeon](
+    1 -> Pigeon("Lucy", 1, Landing),
+    2 -> Pigeon("Bob", 2, Flying(Shitting)),
+    3 -> Pigeon("Carmen", 2, Starting),
+    4 -> Pigeon("Graham", 2, Standing(Pecking)),
+    5 -> Pigeon("All", 2, Landing)
+  )
+}
+
 case class Point(x: Int, y: Int){
   def +(p: Point) = Point(x + p.x, y + p.y)
   def /(d: Int) = Point(x / d, y / d)
